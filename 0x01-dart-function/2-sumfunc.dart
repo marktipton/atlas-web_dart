125c7b8bb7de06a345a1c97728d71fc03e0b3504
marktipton <marktipton95@gmail.com>
2024-10-07 14:25:07 -0500

    added sumfunc calling other functions

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,15 @@
+int add(int a, int b) {
+  return (a + b);
+}
+
+int sub(int a, int b) {
+  return (a - b);
+}
+
+String showFunc(int a, int b) {
+  int sum = add(a, b);
+  int difference = sub(a, b);
+  String addString = 'Add $a + $b = $sum';
+  String subString = 'Sub $a + $b = $difference';
+  return ('$addString\n$subString');
+}
