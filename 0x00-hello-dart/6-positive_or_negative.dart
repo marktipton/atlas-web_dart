dd015d790fca372f93deebc567693f12c253cab7
marktipton <marktipton95@gmail.com>
2024-10-07 11:13:10 -0500

    checking sign of arg

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,1 +1,10 @@
-
+void main(List<String> arg) {
+  int n = int.parse(arg[0]);
+  if (n < 0) {
+    print('$n is negative');
+  } else if (n == 0) {
+    print('$n is zero');
+  } else {
+    print('$n is positive');
+  }
+}
