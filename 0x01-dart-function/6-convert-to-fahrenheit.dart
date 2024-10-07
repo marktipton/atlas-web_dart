af6776b447db91a9944a1870f7b24fc8f69c3fb4
marktipton <marktipton95@gmail.com>
2024-10-07 15:56:23 -0500

    used map to convert C to F but map creates an iterable so it must be converted back to a List using toList()

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,4 @@
+List<double> convertToF(List<double> temperaturesInC) {
+// temperaturesInC.map((temp) => ((temp * 1.8) + 32));
+  return temperaturesInC.map((temp) => ((temp * 1.8) + 32)).toList();
+}
