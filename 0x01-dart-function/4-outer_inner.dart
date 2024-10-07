6fdcfe5cd7725ef4838521b2336d03476e1940fd
marktipton <marktipton95@gmail.com>
2024-10-07 15:11:03 -0500

    made name abbreviation using a list

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,8 @@
 void outer(String name, String id) {
+  List<String> splitName = name.split(' ');
+  String abbrevName = '${splitName[1][0]}.${splitName[0]}';
   String inner() {
-    return ('Hello Agent $name your id is $id');
+    return ('Hello Agent $abbrevName your id is $id');
   }
 
   print(inner());
