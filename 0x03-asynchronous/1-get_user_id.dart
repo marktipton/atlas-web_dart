a4356611a60ace1424b31286ef3c7ea647019300
marktipton <marktipton95@gmail.com>
2024-10-13 15:30:43 -0500

    converted to json to get ID only

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,9 +1,11 @@
 import '1-util.dart';
+import 'dart:convert';
 
 Future<String> getUserId() async {
   try {
     String userString = await fetchUserData();
-    return userString;
+    Map<String, dynamic> userData = jsonDecode(userString);
+    return userData['id'];
   } catch (e) {
     print('Error fetching users data: $e');
     return '';
