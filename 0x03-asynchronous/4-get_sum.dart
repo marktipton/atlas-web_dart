eb4f4b35d81e2180368ebbda59328e7454d8c61b
marktipton <marktipton95@gmail.com>
2024-10-13 16:17:35 -0500

    found total price succesfully

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,15 +1,24 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<List> calculateTotal() async {
+Future<double> calculateTotal() async {
   try {
+    // find user id
     String userData = await fetchUserData();
     Map<String, dynamic> userJson = jsonDecode(userData);
     String userId = userJson['id'];
+    // find orders for that user
     String userOrdersData = await fetchUserOrders(userId);
     List<dynamic> userOrders = jsonDecode(userOrdersData);
-    return userOrders;
+    double totalPrice = 0;
+    // calculate prices of each order
+    for (var order in userOrders) {
+      String priceData = await fetchProductPrice(order);
+      double price = double.parse(priceData);
+      totalPrice += price;
+    }
+    return totalPrice;
   } catch (e) {
-    return ([]);
+    return (-1);
   }
 }
