9774cc29814b8edbe6b00aaf20f9bde72338ff4f
marktipton <marktipton95@gmail.com>
2024-10-13 11:16:27 -0500

    added password check for length

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,12 @@
+class Password {
+  String? password;
+
+  Password({this.password});
+
+  bool isValid() {
+    if (password != null && password!.length > 7 && password!.length < 17) {
+      return true;
+    }
+    return false;
+  }
+}
