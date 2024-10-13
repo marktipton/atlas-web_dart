e4e9a5920eeb20484f83fa35019e591020b0425f
marktipton <marktipton95@gmail.com>
2024-10-13 16:19:07 -0500

    return num instead of double

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
     // find user id
     String userData = await fetchUserData();
