20e7e2bc196be87029cbf55a36bfb964c8b90a65
marktipton <marktipton95@gmail.com>
2024-10-13 12:52:49 -0500

    extended password class to User

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,45 @@
+import '6-password.dart';
+
+class User {
+  String? name;
+  int? age;
+  double? height;
+  int? id;
+  String? user_password;
+
+  User({this.name, this.age, this.height, this.id, this.user_password});
+
+  String showName() {
+    return ("Hello ${this.name ?? 'User'}");
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+      user_password: userJson['password'] ?? '',
+    );
+  }
+
+  bool isPasswordValid() {
+    if (user_password == null) return false;
+    Password passwordInstance = Password(password: user_password);
+    return passwordInstance.isValid();
+  }
+
+  @override
+  String toString() {
+    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${isPasswordValid()})';
+  }
+}
---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,40 @@
+class Password {
+  String? _password;
+
+  Password({String? password}) : _password = password;
+
+  bool isValid() {
+    if (_password != null &&
+        _password!.length > 7 &&
+        _password!.length < 17 &&
+        checkCharacters()) {
+      return true;
+    }
+    return false;
+  }
+
+  bool checkCharacters() {
+    final RegExp hasUppercase = RegExp(r'[A-Z]');
+    final RegExp hasLowercase = RegExp(r'[a-z]');
+    final RegExp hasDigit = RegExp(r'[0-9]');
+
+    return hasDigit.hasMatch(_password!) &&
+        hasLowercase.hasMatch(_password!) &&
+        hasUppercase.hasMatch(_password!);
+  }
+
+  // getter
+  String? get password {
+    return _password;
+  }
+
+  // setter
+  set password(String? value) {
+    _password = value;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: ${_password ?? 'N/A'}';
+  }
+}
