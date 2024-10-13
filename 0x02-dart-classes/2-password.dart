8acf827f684cb40d3bf84855de40bcfcfd6af64b
marktipton <marktipton95@gmail.com>
2024-10-13 11:30:23 -0500

    added check for characters

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -4,10 +4,23 @@ class Password {
   Password({this.password});
 
   bool isValid() {
-    if (password != null && password!.length > 7 && password!.length < 17) {
+    if (password != null &&
+        password!.length > 7 &&
+        password!.length < 17 &&
+        checkCharacters()) {
       return true;
     }
     return false;
+  }
+
+  bool checkCharacters() {
+    final RegExp hasUppercase = RegExp(r'[A-Z]');
+    final RegExp hasLowercase = RegExp(r'[a-z]');
+    final RegExp hasDigit = RegExp(r'[0-9]');
+
+    return hasDigit.hasMatch(password!) &&
+        hasLowercase.hasMatch(password!) &&
+        hasUppercase.hasMatch(password!);
   }
 
   @override
